20446cefaa5d4a90c8ab0dee659b06aaa78c9f4e
Mike Little <mmlittle@yahoo.com>
2025-05-29 10:51:35 -0500

    Initial Commit

---
+++ MapsChallenge/MapsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsChallengeApp.swift
+//  MapsChallenge
+//
+//  Created by llamaman on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MapsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapsChallengeTests/MapsChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapsChallengeTests.swift
+//  MapsChallengeTests
+//
+//  Created by llamaman on 5/29/25.
+//
+
+import Testing
+@testable import MapsChallenge
+
+struct MapsChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
